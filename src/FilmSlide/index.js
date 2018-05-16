55c6c60613b0bbf6d6856e6ebe3cdb1bc70bf19b
zlydenko <anton.zlydenko@gmail.com>
2018-05-16 12:32:27 +0300

    small cosmetic changes

--- src/FilmSlide/index.js
+++ src/FilmSlide/index.js
@@ -4,6 +4,8 @@ import { Wrapper, Btn } from "./styled";
 import SingleTrailer from "../SingleTrailer";
 import Synopsis from "../Synopsis";
 
+const BuyBtn = () => <Btn>{"buy tickets"}</Btn>;
+
 export default class FilmSlide extends Component {
   state = {
     infoExtended: false
@@ -16,26 +18,29 @@ export default class FilmSlide extends Component {
   render() {
     const { trailer, title, caption, tags, cast, score } = this.props;
     const { infoExtended } = this.state;
+
+    const trailerProps = {
+      trailerSource: trailer,
+      infoOpened: infoExtended,
+      clickFn: this.openFullInfo
+    };
+
+    const synopsisProps = {
+      opened: infoExtended,
+      clickFn: this.openFullInfo,
+      info: {
+        title,
+        tags,
+        cast,
+        caption,
+        score
+      }
+    };
+
     return (
       <Wrapper>
-        <SingleTrailer
-          trailerSource={trailer}
-          infoOpened={infoExtended}
-          clickFn={this.openFullInfo}
-        />
-        <Synopsis
-          opened={infoExtended}
-          clickFn={this.openFullInfo}
-          info={{
-            title,
-            tags,
-            cast,
-            caption,
-            score
-          }}
-        >
-          <Btn filmInfoOpened={infoExtended}>{"buy tickets"}</Btn>
-        </Synopsis>
+        <SingleTrailer {...trailerProps} />
+        <Synopsis {...synopsisProps}>{infoExtended && <BuyBtn />}</Synopsis>
       </Wrapper>
     );
   }
--- src/FilmSlide/styled.js
+++ src/FilmSlide/styled.js
@@ -1,12 +1,22 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 export const Wrapper = styled.div`
   width: 100%;
   position: relative;
 `;
 
+const moveUp = keyframes`
+  0% {
+    transform: translate(0,10em);
+  }
+
+  100% {
+    transform: translate(0,0);
+  }
+`;
+
 export const Btn = styled.button`
-  transition: transform 0.9s;
+  //transition: transform 0.9s;
   display: block;
   width: 40%;
   padding: 0.5em;
@@ -21,8 +31,9 @@ export const Btn = styled.button`
   cursor: pointer;
   margin: 0em auto;
   opacity: 0.9;
-  height: ${props => (props.filmInfoOpened ? `auto` : `0em`)};
-  ${props => (props.filmInfoOpened ? null : `transform: translate(0,10em);`)};
+  transform: translate(0, 10em);
+
+  animation: 0.5s ${moveUp} linear 1 forwards;
 
   &:hover {
     opacity: 0.85;
